275fe3f42d62809d698b8e9cc8d4e65feec58f65
Daniel-Quistan <danielfernandesdfo@gmail.com>
2020-03-23 18:02:40 -0300

    Created the get_list_of_movies, get_movie_data and change_windows functions.

--- imdb_crawler.py
+++ imdb_crawler.py
@@ -7,15 +7,24 @@ class imdb_bot():
     the desired information.
     """
 
-    def __init__(self, n_movies, headless=False):
+
+    def __init__(self, n_movies=30, headless=False):
         """
         Initialize the imdb_bot class, creating all variables required
         for the bot, received as input.
         """
 
-        self.headless = headless # Checks if it shows the browser or not
-        self.driver = self.driver_config() # Creates the chromedriver used
-        self.n_movies = n_movies # Set the number of movies to crawl
+        self.headless = headless # Checks if it shows the browser or not.
+        self.movies_url = 'https://www.imdb.com/chart/top/'
+        self.driver = self.driver_config() # Creates the chromedriver used.
+        self.driver.get(self.movies_url) # Go to IMDB top 250 page.
+        self.n_movies = n_movies # Set the number of movies to crawl.
+        self.list_of_movies = self.get_list_of_movies()
+        movie_names = []
+        for movie in self.list_of_movies:
+            movie_names.append(movie.text)
+        print(movie_names)
+        self.driver.quit()
 
 
 
@@ -30,6 +39,17 @@ class imdb_bot():
             chrome_options.add_argument('headless')
         driver =  webdriver.Chrome(options=chrome_options)
         return driver
+
+
+    def get_list_of_movies(self):
+        """
+        Get's the page elements of the number of movies specified.
+        """
+        list_of_movies = self.driver.find_elements_by_xpath(
+            '//*/tbody[@class="lister-list"]/tr'
+            )
+        list_of_movies = list_of_movies[0:self.n_movies]
+        return list_of_movies
 
 
 if __name__ == '__main__':
